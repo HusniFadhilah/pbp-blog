faba22cb1e6b7e9fcf94a098b7ac293935a201e9
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-14 21:00:56 +0700

    Membuat migrasi tabel kategori

---
+++ app/Database/Migrations/2020-10-14-135359_kategori.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Kategori extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'idkategori'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'nama'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'created_at' => [
+				'type'           => 'DATETIME',
+				'null'     		 => true,
+			],
+			'updated_at' => [
+				'type'           => 'DATETIME',
+				'null'     		 => true,
+			],
+		]);
+		$this->forge->addKey('idkategori', true);
+		$this->forge->createTable('kategori');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('kategori');
+	}
+}
