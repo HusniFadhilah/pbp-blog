5ac5a8d36894043e07f0816e6514f119a1d0c709
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-19 16:56:57 +0700

    Menambahkan assets datatables ke template penulis

--- app/Views/templates/penulis/header.php
+++ app/Views/templates/penulis/header.php
@@ -9,6 +9,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <!-- Font Awesome -->
+    <link href="/assets/fonts/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this page -->
+    <!-- DataTales -->
+    <link href="/assets/vendor/datatables/media/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <title>Penulis</title>
 </head>
 
