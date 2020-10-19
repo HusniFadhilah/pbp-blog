1a9e73efd4a5027b6fd59accffde168381b1d929
rochmad.wa@gmail.com <rochmad.wa@gmail.com>
2020-10-19 17:08:55 +0700

    Menambah kategori

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -2,13 +2,105 @@
 
 namespace App\Controllers;
 
+use App\Models\KategoriModel;
+
 class Kategori extends BaseController
 {
-    public function index()
+    protected $kategoriModel;
+
+    public function __construct()
     {
-        return view('admin/kategori/kategori_data');
+        $this->kategoriModel = new KategoriModel();
     }
 
-    //--------------------------------------------------------------------
+    public function index()
+    {
+        $kategori = $this->kategoriModel->findAll();
 
+        $data = [
+            'kategori' => $kategori
+        ];
+
+        return view('admin/kategori/kategori_data', $data);
+    }
+
+    public function create()
+    {
+//        session();
+        $data = [
+            'judul' => 'Form Tambah Kategori',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('admin/kategori/kategori_form_add', $data);
+    }
+
+    public function save()
+    {
+        if (!$this->validate([
+            'nama' => [
+                'rules' => 'required|is_unique[kategori.nama]',
+                'errors' => [
+                    'required' => '{field} kategori harus diisi.',
+                    'is_unique' => '{field} kateori sudah ada.'
+                ]
+            ]
+        ])){
+            $validation = \Config\Services::validation();
+
+            return redirect()->to('create')->withInput()->with('validation', $validation);
+        }
+
+        $this->kategoriModel->save([
+            'nama' => $this->request->getVar('nama')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan');
+        
+        return redirect()->to('/kategori');
+    }
+
+    public function delete($id){
+        $this->kategoriModel->delete($id);
+
+        session()->setFlashdata('pesan', 'Data berhasil dihapus');
+
+        return redirect()->to('/kategori');
+    }
+
+    public function edit($id){
+        $data = [
+            'judul' => 'Form Ubah Kategori',
+            'validation' => \Config\Services::validation(),
+            'kategori' => $this->kategoriModel->find($id)
+        ];
+
+        return view('admin/kategori/kategori_form_edit', $data);
+    }
+
+    public function update($id){
+        if (!$this->validate([
+            'nama' => [
+                'rules' => 'required|is_unique[kategori.nama]',
+                'errors' => [
+                    'required' => '{field} kategori harus diisi.',
+                    'is_unique' => '{field} kateori sudah ada.'
+                ]
+            ]
+        ]))
+        {
+            $validation = \Config\Services::validation();
+
+            return redirect()->to('/kategori/edit/'.$id)->withInput()->with('validation', $validation);
+        }
+
+        $this->kategoriModel->save([
+            'idkategori' => $id,
+            'nama' => $this->request->getVar('nama')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil diubah.');
+
+        return redirect()->to('/kategori');
+    }
 }
--- app/Views/admin/kategori/kategori_data.php
+++ app/Views/admin/kategori/kategori_data.php
@@ -1,11 +1,48 @@
 <?= $this->extend('templates/admin/template') ?>
 <?= $this->section('contentadmin') ?>
 
-<!-- ISI KONTEN -->
-<!-- Taruh konten di bawah sini -->
-<h1>Halaman Kategori Data (Admin)</h1>
+    <!-- ISI KONTEN -->
+    <!-- Taruh konten di bawah sini -->
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Halaman Kategori Data (Admin)</h1>
+                <div>
+                    <a class="btn btn-primary my-3" href="/kategori/create">+ Add Category</a>
+                </div>
+                <?php if (session()->getFlashdata('pesan')): ?>
+                    <div class="alert alert-success" role="alert">
+                        <?= session()->getFlashdata('pesan'); ?>
+                    </div>
+                <?php endif; ?>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Category ID</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Operations</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($kategori as $k) : ?>
+                    <tr>
+                        <td><?= $k['idkategori'] ; ?></td>
+                        <td><?= $k['nama']; ?></td>
+                        <td>
+                            <a type="button" class="btn btn-warning" href="/kategori/edit/<?= $k['idkategori']; ?>">Edit</a>
+                            <a type="button" class="btn btn-danger" href="/kategori/delete/<?= $k['idkategori']; ?>" onclick="return confirm('Apakah Anda yakin?');">
+                                Delete
+                            </a>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
-<!-- AKHIR ISI KONTEN -->
+    <!-- AKHIR ISI KONTEN -->
 
 <?= $this->endSection() ?>
