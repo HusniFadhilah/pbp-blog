3eec5542f4dabbc96c01f91ab37e5dd7bf9f0e8c
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-22 18:52:10 +0700

    Modifikasi halaman dashboard admin

--- app/Views/admin/dashboard/dashboard.php
+++ app/Views/admin/dashboard/dashboard.php
@@ -4,12 +4,132 @@
 <!-- ISI KONTEN -->
 <!-- Taruh konten di bawah sini -->
 <div class="container">
+
+    <div>
+        <h1 class="h3 mb-2 text-gray-800">Halaman Dashboard Admin</h1>
+    </div>
+    <!-- Jumlah Penulis -->
     <div class="row">
-        <div class="col">
-            <h1 class="h3 mb-2 text-gray-800">Halaman Dashboard Admin</h1>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Penulis</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">1000</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Jumlah Kategori -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Kategori</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">1000</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-folder fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Jumlah Artikel -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Artikel</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-newspaper fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Jumlah Komentar -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Jumlah Komentar</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-comments fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <!-- Area Chart -->
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Komentar Artikel</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="myAreaChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Pie Chart Kategori Artikel</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="chart-pie pt-4 pb-2">
+                        <canvas id="myPieChart"></canvas>
+                    </div>
+                    <div class="mt-4 text-center small">
+                        <!-- Tolong dihubungkan dengan backened -->
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-primary"></i> Direct
+                        </span>
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-success"></i> Social
+                        </span>
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-info"></i> Referral
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- Page level plugins -->
+<script type="text/javascript" src="/assets/vendor/chart.js/Chart.min.js"></script>
+<script type="text/javascript" src="/assets/js/demo/chart-area-demo.js"></script>
+<script type="text/javascript" src="/assets/js/demo/chart-pie-demo.js"></script>
 
 <!-- AKHIR ISI KONTEN -->
 
