1dc5733d8bb4cbb783897bcadc8da3d07c446557
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-22 15:02:28 +0700

    Edit file header template public

--- app/Views/templates/public/header.php
+++ app/Views/templates/public/header.php
@@ -13,6 +13,65 @@
     <link href="<?= base_url() ?>/assets/fonts/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
 
     <title>Public</title>
+
+    <!-- CUSTOM STYLE -->
+    <style>
+        .comment {
+            position: relative;
+        }
+
+        .comment-img {
+            border-radius: 50%;
+            max-width: 50px;
+            margin-right: 15px;
+        }
+
+        .comment h5 {
+            margin: 0;
+            padding: 0;
+        }
+
+        .comment h5,
+        .comment img {
+            display: table-cell;
+            vertical-align: top;
+        }
+
+        .comment h5 a {
+            color: #000;
+            font-size: 18px;
+        }
+
+        .comment h5 a:hover {
+            color: #ff9800;
+            transition: all 0.3s;
+            text-decoration: none;
+        }
+
+        .comment .reply {
+            font-size: 16px;
+            margin-left: 10px;
+        }
+
+        .comment .comment-reply {
+            padding-left: 40px;
+        }
+
+        .comment a i.fa {
+            font-size: 12px;
+        }
+
+        .comment time {
+            color: #777;
+            font-size: 15px;
+        }
+
+        .clearfix::after {
+            display: block;
+            clear: both;
+            content: "";
+        }
+    </style>
 </head>
 
 <body>
