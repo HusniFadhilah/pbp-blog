1c223f328a16679745d8111cce4c40f43db991f8
rochmad.wa@gmail.com <rochmad.wa@gmail.com>
2020-10-23 07:50:38 +0700

    Menambah halaman daftar post berdasarkan kategori

--- app/Views/public/post/post_group.php
+++ app/Views/public/post/post_group.php
@@ -3,26 +3,79 @@
 
 <!-- ISI KONTEN -->
 <!-- Taruh konten di bawah sini -->
-<h1>Halaman Group By Kategori</h1>
-    <div class="row article mb-3">
-        <?php foreach ($post as $p) : ?>
-            <div class="col-lg-3">
-                <article class="item">
-                    <div class="card">
-                        <img class="card-img-top" src="assets/img/article/blog-1.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title"><a href="/post/detail/<?= $p["slug"] ?>"><?= $p["judul"] ?></a></h5>
-                            <p class="card-text"><?= $p["isi_post"] ?> <br> kategori <?= $p["idkategori"] ?></p>
-                            <a href="/post/detail/<?= $p["slug"] ?>" class="btn btn-primary btn-sm">More<i class="fa fa-arrow-right pl-1"></i></a>
-                            <span class="float-right"><small><i class="far fa-calendar mr-1"></i> <?= $p['tgl_insert'] ?> </small></span>
+<div class="slider-main h-sm-auto pos-relative pt-95 pb-25">
+    <div class="img-bg bg-1 bg-layer-4"></div>
+    <div class="container-fluid h-100 mt-xs-50">
+
+            <div class="dplay-tbl">
+                <div class="dplay-tbl-cell color-white text-center">
+
+                    <h1 class="ptb-50"><b>Kategori <?= ucfirst($kategori['nama']) ?></b></h1>
+
+                </div><!-- dplay-tbl-cell -->
+            </div><!-- dplay-tbl -->
+    </div><!-- container -->
+</div><!-- slider-main -->
+
+<section class="bg-1-white ptb-0">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-md-12 col-lg-10 ptb-50 pr-30 pr-md-15">
+
+
+                <div class="row">
+                    <?php if (count($post) > 0) : ?>
+                        <?php foreach ($post as $p) : ?>
+                            <!-- START OF SECOND PARA -->
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-30">
+                                <div class="card h-100 h-xs-500x">
+                                    <div class="sided-half sided-xs-half h-100 bg-white">
+
+                                        <!-- SETTING IMAGE WITH bg-2 -->
+                                        <div class="s-left w-50 w-xs-100 h-100 h-xs-50 pos-relative">
+                                            <img src="/assets/img/post/<?= $p["file_gambar"] ?>"
+                                                 style="object-fit:cover; width:100%"></img>
+                                        </div>
+
+                                        <div class="s-right w-50 w-xs-100 h-xs-50">
+                                            <div class="plr-25 ptb-25 h-100">
+                                                <div class="dplay-tbl">
+                                                    <div class="dplay-tbl-cell">
+
+                                                        <h5 class="color-ash"><b><?= ucfirst($kategori["nama"]) ?></b></h5>
+                                                        <h4 class="mtb-10"><a href="/post/detail/<?= $p["slug"] ?>">
+                                                                <b><?= $p["judul"] ?></b></a></h4>
+                                                        <ul class="list-li-mr-10 color-lt-black">
+                                                            <li>
+                                                                <i class="mr-5 font-12 ion-ios-calendar-outline"></i><?= indo_date($p["tgl_insert"]) ?>
+                                                            </li>
+                                                            <li>
+                                                                <i class="mr-5 font-12 ion-ios-chatbubble-outline"></i>105
+                                                            </li>
+                                                        </ul>
+                                                    </div><!-- dplay-tbl-cell -->
+                                                </div><!-- dplay-tbl -->
+                                            </div><!-- plr-25 ptb-25 -->
+                                        </div><!-- s-right -->
+                                    </div><!-- sided-half -->
+                                </div><!-- card -->
+                            </div><!-- col-lg-8 col-md-12 -->
+                            <!-- END OF SECOND PARA -->
+                        <?php endforeach ?>
+                    <?php else : ?>
+                        <div class="alert alert-warning">Maaf, artikel kategori <?= ucfirst($kategori['nama']) ?>
+                            tidak ditemukan
                         </div>
-                    </div>
-                </article>
-            </div>
-        <?php endforeach ?>
-    </div>
-    <?= $pager->links('post', 'post_pagination') ?>
-</div>
+                    <?php endif ?>
+
+                </div><!-- row -->
+                <?= $pager->links('post', 'post_pagination') ?>
+            </div><!-- s-left -->
+        </div><!-- sided-80x -->
+    </div><!-- mb-50 -->
+
+</section>
 
 <!-- AKHIR ISI KONTEN -->
 
