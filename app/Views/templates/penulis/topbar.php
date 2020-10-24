a0954680bdbcbfeac583d1725f7b47de489cd53a
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-24 08:58:40 +0700

    Buat link ke halaman home di web admin & penulis

--- app/Views/templates/penulis/topbar.php
+++ app/Views/templates/penulis/topbar.php
@@ -1,49 +1,55 @@
-  <!-- Content Wrapper -->
-  <div id="content-wrapper" class="d-flex flex-column">
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+    <!-- Main Content -->
+    <div id="content">
 
-          <!-- Topbar -->
-          <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-              <!-- Sidebar Toggle (Topbar) -->
-              <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                  <i class="fa fa-bars"></i>
-              </button>
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
 
-              <!-- Topbar Navbar -->
-              <ul class="navbar-nav ml-auto">
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
 
-                  <div class="topbar-divider d-none d-sm-block"></div>
+                <li class="nav-item no-arrow mx-1">
+                    <a class="nav-link" href="/" title="Ke halaman home">
+                        <i class="fas fa-home fa-fw"></i>
+                    </a>
+                </li>
 
-                  <!-- Nav Item - User Information -->
-                  <li class="nav-item dropdown no-arrow">
-                      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
-                          <img class="img-profile rounded-circle" src="/assets/img/user/default.jpg">
-                      </a>
-                      <!-- Dropdown - User Information -->
-                      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in fadeInDown" aria-labelledby="userDropdown">
-                          <a class="dropdown-item" href="/penulis/edit/<?= session()->get('idpenulis') ?>">
-                              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                              Edit Profile
-                          </a>
-                          <a class="dropdown-item" href="/penulis/ubahpassword/<?= session()->get('idpenulis') ?>">
-                              <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
-                              Ubah Password
-                          </a>
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="/authpenulis/logout" data-toggle="modal" data-target="#logoutModal">
-                              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                              Logout
-                          </a>
-                      </div>
-                  </li>
+                <div class="topbar-divider d-none d-sm-block"></div>
 
-              </ul>
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
+                        <img class="img-profile rounded-circle" src="/assets/img/user/default.jpg">
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in fadeInDown" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="/penulis/edit/<?= session()->get('idpenulis') ?>">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Edit Profile
+                        </a>
+                        <a class="dropdown-item" href="/penulis/ubahpassword/<?= session()->get('idpenulis') ?>">
+                            <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Ubah Password
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="/authpenulis/logout" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+                    </div>
+                </li>
 
-          </nav>
-          <!-- End of Topbar -->
-          <!-- Begin Page Content -->
-          <div class="container-fluid">
+            </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
