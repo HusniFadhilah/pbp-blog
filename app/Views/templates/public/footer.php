d7b7451d5162d0e14f54fcfa5cee20fc4939d7ca
HusniFadhilah <husnfadhilah62@gmail.com>
2020-10-24 08:17:54 +0700

    Perbaiki footer public

--- app/Views/templates/public/footer.php
+++ app/Views/templates/public/footer.php
@@ -1,11 +1,11 @@
-<footer class="bg-191 color-ash pt-50 pb-20 text-left center-sm-text">
+<footer class="bg-191 color-ash pt-10 pb-0 text-center center-sm-text">
 
     <div class="container-fluid">
         <div class="row">
 
             <div class="col-lg-1"></div>
 
-            <div class="col-md-4 col-lg-6 mb-30">
+            <div class="col-12 mb-30">
                 <div class="card h-100">
                     <div class="dplay-tbl">
                         <div class="dplay-tbl-cell">
@@ -31,21 +31,6 @@
 
 
 
-                        </div><!-- dplay-tbl-cell -->
-                    </div><!-- dplay-tbl -->
-                </div><!-- card -->
-            </div><!-- col-lg-4 col-md-6 -->
-
-            <div class="col-md-4 col-lg-2 mb-30 text-left">
-                <div class="card h-100">
-                    <div class="dplay-tbl">
-                        <div class="dplay-tbl-cell">
-                            <form class="form-block form-brdr-b mx-w-400x m-auto">
-
-                                <input class="color-white ptb-15 center-sm-text" type="text" placeholder=" Your Email">
-                                <button class="mt-30 btn-brdr-grey color-ash w-100 text-center" type="submit">SUBSCRIBE</button>
-
-                            </form>
                         </div><!-- dplay-tbl-cell -->
                     </div><!-- dplay-tbl -->
                 </div><!-- card -->
